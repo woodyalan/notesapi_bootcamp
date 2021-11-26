b9dc5319251c878228eb70461af3b1f8e106b727
Alan Santos <alan.afs89@gmail.com>
2021-11-25 21:16:55 -0300

    Rota de usuário realizando as operações

--- rotas/usuario.js
+++ rotas/usuario.js
@@ -2,22 +2,59 @@ const { Router } = require("express");
 const { Usuario } = require("../bd");
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/:id?", (req, res) => {
   res.send([]);
 });
 
 router.post("/", async (req, res) => {
-  const usuario = await Usuario.create(req.body);
+  const { nome, email, senha } = req.body;
+
+  const [usuario, novoRegistro] = await Usuario.findOrCreate({
+    defaults: {
+      nome,
+      email,
+      senha,
+    },
+    where: {
+      email,
+    },
+  });
+
+  console.log(novoRegistro);
 
   res.send(usuario);
 });
 
-router.put("/:id", (req, res) => {
-  res.send({});
+router.put("/:id", async (req, res) => {
+  const { nome, email, senha } = req.body;
+  const { id } = req.params;
+
+  const usuario = await Usuario.update(
+    {
+      nome,
+      email,
+      senha,
+    },
+    {
+      where: {
+        id,
+      },
+    }
+  );
+
+  res.send(usuario);
 });
 
-router.delete("/:id", (req, res) => {
-  res.send({});
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+
+  const usuario = await Usuario.destroy({
+    where: {
+      id,
+    },
+  });
+
+  res.send({ usuario });
 });
 
 module.exports = router;
