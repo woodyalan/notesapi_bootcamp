d90178e41f33fa38461e26b181f9285f96a7c814
Alan Santos <alan.afs89@gmail.com>
2021-12-02 22:43:14 -0300

    Configuração do CORS (ainda não funcionou) Configurações para gerar o token JWT

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const porta = 3000;
 
+app.use(cors({ origin: "http://grupointegrado.br" }));
 app.use(express.json());
 
 const usuario = require("./rotas/usuario");
